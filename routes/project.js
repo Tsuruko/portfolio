c6321df4da9d345f4b36aa6bda81fc2935a3cda8
Monica <mcl018@ucsd.edu>
2015-05-11 22:25:37 -0700

    cse190 assignment 2

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,8 @@
 var projectsGraphics = require('../projectsGraphics.json');
 var projectsGeneral = require('../projectsGeneral.json');
 
-var cse190_img = require('../CSE190.json');
+var cse190_1 = require('../CSE190_1.json');
+var cse190_2 = require('../CSE190_2.json');
 
 
 exports.viewProject = function(req, res) {
@@ -22,5 +23,11 @@ exports.viewProjectSection = function(req, res) {
 	var section = req.params.section;
 	var url = name + "/" + section;
 
-	res.render(url, cse190_img);
+	if (section == "Assignment1") {
+		res.render(url, cse190_1);
+	} else {
+		res.render(url, cse190_2);
+	}
+
+
 }
