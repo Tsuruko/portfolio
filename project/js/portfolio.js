fac29117cd8a1a6a6a2d15c58f41b1469296c1af
Monica <mcl018@ucsd.edu>
2018-03-30 12:12:40 -0600

    update format to new website

---
+++ project/js/portfolio.js
@@ -0,0 +1,124 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+
+}
+
+function popup(e) {
+	var popupImg = document.getElementById("imgPopup");
+	var imgID = e.getAttribute("data-imgSrc");
+	popupImg.src = document.getElementById(imgID).src;
+	var popupImgWidth = popupImg.width;
+	
+	if (popupImgWidth > 1000 || popupImgWidth == 0) {
+		popupImgWidth = 1000;
+	}
+
+	$('#myModalImg').find('.modal-dialog').css({
+		width: popupImgWidth
+	});
+}
+
+
+
+/* ---------------------------------------------------
+	NavBar functions
+----------------------------------------------------- */
+
+var subOptionclicked = false;
+
+//Change active list item
+function activateMenuItem () {
+	// If this isn't already active
+	if (!$(this).hasClass("active")) {
+		// Remove the class from anything that is active
+		$(".menuOption").removeClass("active");
+		// And make this active
+		$(this).addClass("active");
+	}
+	if (!subOptionclicked) {
+		$(".subOption").removeClass("active");
+	}
+	subOptionclicked = false;
+}
+
+function activateSubItem () {
+	// If this isn't already active
+	if (!$(this).hasClass("active")) {
+		// Remove the class from anything that is active
+		$(".subOption").removeClass("active");
+		// And make this active
+		$(this).addClass("active");
+	}
+	subOptionclicked = true;
+}
+
+function removeFocus() {
+	$(this).blur();
+}
+
+//check which navigation bar to use, mobile or full version
+function checkSize() {
+ 	if ($("#sideHeader").css("display") == "none") {
+
+		$('#navbar').removeClass('sideBarContainer');
+		$('#navbar').addClass('navbar navbar-custom navbar-fixed-top');
+		$('#optionsList').addClass('collapse navbar-collapse');
+		$('#optionsListType').removeClass('list-unstyled components');
+		$('#optionsListType').addClass('nav navbar-nav');
+
+	} else {
+		$('#navbar').removeClass('navbar navbar-custom navbar-fixed-top');
+		$('#navbar').addClass('sideBarContainer');
+		$('#optionsList').removeClass('collapse navbar-collapse');
+		$('#optionsListType').removeClass('nav navbar-nav');
+		$('#optionsListType').addClass('list-unstyled components');
+	}
+
+	console.log($("#navbar").parent().html());
+}
+
+// Add smooth scrolling animation
+function smoothScroll (event) {
+	// Make sure this.hash has a value before overriding default behavior
+	if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+
+		// Store hash
+		var hash = this.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 800, function(){
+
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		});
+	}
+}
+
+
+/* ---------------------------------------------------
+	Collpased NavBar functions
+----------------------------------------------------- */
+function open_menu() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("menuHeader").style.display = "none";
+}
+function close_menu() {
+    document.getElementById("mySidebar").style.width = "300px"; 
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("menuHeader").style.display = "block";
+}
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-
-exports.view = function(req, res){
-	res.render('index');
-}
-
-exports.contact = function(req, res) {
-	res.render('contact');
-}
